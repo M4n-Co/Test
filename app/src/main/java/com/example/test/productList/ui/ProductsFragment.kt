5ea2e1af4eaba63fae0fa9eecb31ae3acac9a050
M4n-Co <122502579+M4n-Co@users.noreply.github.com>
2024-02-27 15:03:17 -0600

    correciones XD

--- app/src/main/java/com/example/test/productList/ui/ProductsFragment.kt
+++ app/src/main/java/com/example/test/productList/ui/ProductsFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -45,6 +46,10 @@ class ProductsFragment : Fragment() {
             if (it != null){
                 initRecycler(it.products)
             }
+        }
+        mViewModel.isLoading.observe(viewLifecycleOwner){
+            mBinding.pbProducts.isVisible = it
+            mBinding.rvProducts.isVisible = !it
         }
     }
 
